200205216bd3454c20b45394228de4b25579aaa2
Joseph Rollins <rollins.joseph@gmail.com>
2023-03-25 22:33:23 -0700

    Turn evil mode off for vterm

--- config.el
+++ config.el
@@ -74,3 +74,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(after! evil-collection-vterm
+  (evil-set-initial-state 'vterm-mode 'emacs))
